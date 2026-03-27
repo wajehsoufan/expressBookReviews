ad43b950d3648f5780d4138bfaaac2a0f3ba4435
wajehsoufan <wajeh.soufan@gmail.com>
2026-03-27 12:08:20 -0400

    Task12

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -91,23 +91,29 @@ public_users.get('/isbn/:isbn',function (req, res) {
 });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-    const author = decodeURIComponent(req.params.author); // get author from URL
-    const bookKeys = Object.keys(books); // get all ISBN keys
-    const result = [];
+public_users.get('/axios-author-async/:author', async (req, res) => {
+    const author = decodeURIComponent(req.params.author); // decode URL spaces
 
-    // Iterate through books and find matches
-    for (let key of bookKeys) {
-        if (books[key].author === author) {
-            result.push(books[key]);
+    try {
+        const response = await axios.get('http://localhost:5000/'); // fetch all books
+        const books = response.data;
+        const result = [];
+        const bookKeys = Object.keys(books);
+
+        for (let key of bookKeys) {
+            if (books[key].author === author) {
+                result.push(books[key]);
+            }
         }
-    }
 
-    if (result.length > 0) {
-        return res.status(200).send(JSON.stringify(result, null, 4));
-    } else {
-        return res.status(404).json({ message: "No books found by this author" });
+        if (result.length > 0) {
+            res.status(200).send(result);
+        } else {
+            res.status(404).json({ message: "No books found by this author" });
+        }
+
+    } catch (error) {
+        res.status(500).json({ message: "Error fetching books", error: error.message });
     }
 });
 
